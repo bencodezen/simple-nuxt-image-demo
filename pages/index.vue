1c3389c29b5f6ddefda99faa4132965472b9d661
Ben Hong <ben@bencodezen.io>
2021-06-11 11:41:28 -0400

    feature: add space image

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,41 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        simple-nuxt-image-demo
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <nuxt-img
+      src="/img/space.jpg"
+      alt="Space by Jeremy Thomas on Unsplash"
+      sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw"
+    />
+    <p>
+      Photo by
+      <a
+        href="https://unsplash.com/@jeremythomasphoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+      >
+        Jeremy Thomas
+      </a>
+      on
+      <a
+        href="https://unsplash.com/s/photos/space?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+      >
+        Unsplash
+      </a>
+    </p>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
 .container {
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   text-align: center;
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
