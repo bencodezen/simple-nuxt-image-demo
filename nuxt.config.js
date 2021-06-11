f576e1fb053adafecfc7558f37b62dc59b106e63
Ben Hong <ben@bencodezen.io>
2021-06-11 11:20:45 -0400

    config: add nuxt image

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxt/image'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
